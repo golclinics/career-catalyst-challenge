199ec2a7918eed489518e6fd8c6d69ceefc17640
Ebeneza Goyayi <ebenleonard@users-MacBook-Pro.local>
2022-02-18 18:12:05 +0300

    first commit

---
+++ count_zeros.py/challengev1.py
@@ -0,0 +1,6 @@
+def add_num(a,b):
+    sum=a+b;
+    return sum; 
+num1=5  
+num2=4
+print("The sum is",add_num(num1,num2))
---
+++ count_zeros.py/challengev3.py
@@ -0,0 +1,3 @@
+def my_function(minutes):
+    return minutes*60
+print(my_function(3))
