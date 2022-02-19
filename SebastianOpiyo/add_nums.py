ebf0ec90a2a4335055d9b2899560c6d3d657e511
Sebastian Opiyo <sebamopale@gmail.com>
2022-02-19 13:25:48 +0300

    Add sum function.

---
+++ SebastianOpiyo/add_nums.py
@@ -0,0 +1,8 @@
+def add_two_nums(x:int, y:int):
+    return x + y
+
+
+if __name__ == "__main__":
+    print(f'6 + 6 = {add_two_nums(6, 6)}')
+    print(f'10 + 6 = {add_two_nums(10, 6)}')
+    print(f'235 + 6 = {add_two_nums(235, 6)}')
