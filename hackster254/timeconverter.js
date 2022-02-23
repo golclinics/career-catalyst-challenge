8cdfe543f06b58e5be269bf3665fe802cfe809ec
karira <43937812+hackster254@users.noreply.github.com>
2022-02-23 12:25:28 +0300

    Update timeconverter.js

--- hackster254/timeconverter.js
+++ hackster254/timeconverter.js
@@ -1,7 +1,7 @@
 function myFunction(minutes) {
     if (Number.isInteger(minutes)) {
         var seconds = minutes * 60
-        console.log(seconds)
+        //console.log(seconds)
         return seconds;
     }
     return 'Enter an integer'
