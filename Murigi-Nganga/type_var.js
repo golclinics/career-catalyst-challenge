430a6fb53889cb70d2fd94b54c652aedcaed95a2
Murigi-Nganga <murigigracious@students.uonbi.ac.ke>
2022-03-02 16:53:19 +0300

    ADC Entry Challenge

---
+++ Murigi-Nganga/type_var.js
@@ -0,0 +1,11 @@
+// Entry Challenge V2
+
+const varType = varName => typeof(varName)
+
+// Tests
+console.log(varType(1))
+console.log(varType(false))
+console.log(varType({}))
+console.log(varType(null))
+console.log(varType('string'))
+console.log(varType(['array']))
