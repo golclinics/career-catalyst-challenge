5ce018cf12cf69984476906188c2b495e94bdb0c
ashioyajotham <victorashioya960@gmail.com>
2022-03-02 14:07:44 +0300

    challenge v1

---
+++ ashioyajotham/add_function.py
@@ -0,0 +1,7 @@
+def add(number1,number2):
+    sum = number1 + number2;
+    return sum;
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+print("The sum of two number is",add(x,y))
