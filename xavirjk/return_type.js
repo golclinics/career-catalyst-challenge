516a76d690b97e595dd3a60a9f389d73ead08c3c
@j_kahuha <johntexavir@gmail.com>
2022-03-03 13:07:02 +0300

    unit tests

--- xavirjk/return_type.js
+++ xavirjk/return_type.js
@@ -2,4 +2,14 @@ function returnType(arg) {
   return typeof arg;
 }
 
-let type = returnType({});
+const test = (arg1, arg2, arg3) => {
+  let type = returnType(arg1);
+  if (type !== arg2)
+    console.log(`test ${arg3} failed expected ${arg2} but received ${arg1} `);
+  else console.log(`test ${arg3} passed`);
+};
+test('', typeof '', 1);
+test({}, typeof {}, 2);
+test(1, typeof 1, 3);
+
+module.exports = returnType;
