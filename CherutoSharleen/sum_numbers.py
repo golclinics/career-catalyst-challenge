8886c1aa80e127e5cd10e21b34c2cdacd49748ab
CHERUTO <cheruto@students.uonbi.ac.ke>
2022-03-04 11:09:09 +0300

    My Solution

---
+++ CherutoSharleen/sum_numbers.py
@@ -0,0 +1,10 @@
+def addition(num1,num2):
+    try:
+        #sums both integers and floats
+        sum = num1 + num2
+        return sum
+    except:
+        print("Please Enter a Valid Number")
+
+
+
