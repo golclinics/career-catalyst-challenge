ddb370c6764c72ea156a89d9cfc869d8edfb62df
JToffee <sawesheila21@outlook.com>
2022-03-05 22:49:19 +0300

    Initial Commit

---
+++ jtoffee/add.js
@@ -0,0 +1,11 @@
+//Function to add tow numbers and return sum
+
+const addition = (num1, num2) => num1 + num2;
+
+console.log(addition(2, 5));
+
+//Test
+
+const test = addition(100, 300) === 400;
+
+console.log(test); //True
