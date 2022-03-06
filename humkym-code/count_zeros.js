474bb9c73d8fea0ea032a2312ad544ba7782c5b1
Humkym <humkymh@outlook.com>
2022-03-06 21:04:13 +0300

    completed the challenge

---
+++ humkym-code/count_zeros.js
@@ -0,0 +1,7 @@
+function addition (num1, num2) {
+    // const num1 = parseInt(prompt('enter your first num'));
+    // const num2 = parseInt(prompt('enter your second num'));
+    return num1 + num2;
+    
+}
+console.log(addition(2,4));
