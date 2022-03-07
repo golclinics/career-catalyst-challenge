ac4d2652e906e0aee62c5c65e1e2c1e7b60e91c7
Clifford Ouma <ochiengclifford2000@gmail.com>
2022-03-07 10:47:14 +0300

    Created a function takes value and returns type

---
+++ Cli4d/TypeIdentifier.js
@@ -0,0 +1,12 @@
+// Entry Challenge V2 - 
+
+function TypeIdentifier(item) {
+    // Checks data type of the item
+let ItemType = typeof item;
+
+// Return the type of the item
+return ItemType
+}
+
+// Test trial
+console.log(TypeIdentifier(45));
