e664a6309f167f18f79a0dad8ef1a97a2cd724cd
Danny <njengadanny@gmail.com>
2022-03-14 00:27:16 +0300

    Entry Challenge V1 modified

--- njengadanny/sum_function.py
+++ njengadanny/sum_function.py
@@ -3,6 +3,6 @@ def addition(addend1,addend2):
     return result
 
 a1 = int(input("fist addend: "))
-a2 = int(input("secodn addend: "))
+a2 = int(input("second addend: "))
 
 print(addition(a1,a2), "is the result of adding ", a1, "and", a2)
