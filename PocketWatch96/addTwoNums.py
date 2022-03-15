bb6f0e6434b9dd9cc1482f09286d9600b7ecb7a1
PocketWatch96 <pmehta@usiu.ac.ke>
2022-03-15 12:37:08 +0300

    AddTwoNums pmehta submission for challenge

---
+++ PocketWatch96/addTwoNums.py
@@ -0,0 +1,8 @@
+def addTwoNums():
+    num1 = float(input("Enter first number: "))
+    print()
+    num2 = float(input("Enter second number: "))
+    print()
+    print("Sum: " + str((num1 + num2)))
+
+addTwoNums()
