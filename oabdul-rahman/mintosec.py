14ecddbee32b28a9f5a030d29c5c47b35a6c2d33
oabdulrahman <oabdul-rahman@usiu.ac.ke>
2022-03-15 12:36:15 +0300

    oabdul-rahman challenge submission

---
+++ oabdul-rahman/mintosec.py
@@ -0,0 +1,8 @@
+days = int(input("Input days: ")) * 3600 * 24
+hours = int(input("Input hours: ")) * 3600
+minutes = int(input("Input minutes: ")) * 60
+seconds = int(input("Input seconds: "))
+
+time = days + hours + minutes + seconds
+
+print("The  amounts of seconds", time)
